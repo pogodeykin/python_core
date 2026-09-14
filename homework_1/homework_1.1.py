5a4aa847931c5aeb054b33555f3e40d6238509b3
pogodeykin <pogodeykin9205@gmail.com>
2026-09-14 19:55:58 +0300

    added homework_1

---
+++ homework_1/homework_1.1.py
@@ -0,0 +1,12 @@
+float_1 = -1.6
+float_2 = 2.99
+print(type(float_1))
+print(type(float_2))
+int_1 = int(float_1)
+int_2 = int(float_2)
+print(int_1)
+print(int_2)
+print(type(int_1))
+print(type(int_2))
+
+
---
+++ homework_1/homework_1.3.py
@@ -0,0 +1,6 @@
+slovo_1 = 'stroka'
+message = "{}ing".format(slovo_1)
+message_1 = f"{slovo_1}ing"
+print(message)
+print(message_1)
+
